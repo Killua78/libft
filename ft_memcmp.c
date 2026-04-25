890dee23d3c10bb875c10803b524d8a9cac574a9
Killua78 <nacimeboubekeur@gmail.com>
2026-04-25 15:25:44 +0200

    mem

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nboubeke <nboubeke@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/25 13:59:03 by nboubeke          #+#    #+#             */
-/*   Updated: 2026/04/25 15:15:47 by nboubeke         ###   ########.fr       */
+/*   Created: 2026/04/25 15:16:20 by nboubeke          #+#    #+#             */
+/*   Updated: 2026/04/25 15:24:33 by nboubeke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *str;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t	lens1;
+	size_t	lens2;
 	int	i;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	lens1 = strlen(s1);
+	lens2 = strlen(s2);
 	i = 0;
-	str = (unsigned char *)s;
 
-	while (i < n)
+	while ((lens1 <= n) && (lens2 <= n) && i < n)
 	{
-		if (str[i] == c)
-			return (&str[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 
-	return (NULL);
+	return (str1[i] - str2[i]);
 }
