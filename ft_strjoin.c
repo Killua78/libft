b1d76020509720cc151d0fe7879c4d61ffab438b
Nacime <nacimeboubekeur@gmail.com>
2026-05-02 18:20:46 +0200

    ft_strtrim à tester

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,27 +1,41 @@
-// 42 header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboubeke <nboubeke@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/02 18:10:27 by nboubeke          #+#    #+#             */
+/*   Updated: 2026/05/02 18:10:40 by nboubeke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	int		lens1;
+	int		lens2;
 
 	i = 0;
 	j = 0;
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!s1 || !s2)
+		return (NULL);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	if (!str)
 		return (NULL);
-	while (i < ft_strlen(s1))
+	while (i < lens1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (j < ft_strlen(s2))
-	{
+	while (j < lens2)
 		str[i++] = s2[j++];
-	}
 	str[i] = '\0';
 	return (str);
 }
